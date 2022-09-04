20d120ffa47cab79ba749d4bb8aeaeb2b0728035
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-09-04 13:17:39 -0400

    Add styles for desktop view of main content

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,36 +6,39 @@ import "./Header.css";
 const Header = ({ navIsOpen, toggleNav, closeNav }) => {
   return (
     <header className="header">
-      <img src={logo} alt="" className="logo" />
-      <button
-        onClick={toggleNav}
-        aria-label={navIsOpen ? "Close Menu" : "Open Menu"}
-      >
-        <img
-          src={navIsOpen ? closeMenu : hamburger}
-          alt=""
-          className="menu-btn-icon"
-        />
-      </button>
-      <nav className={`nav${navIsOpen ? " open" : ""}`}>
-        <ul>
-          <li>
-            <a href="/" onClick={closeNav}>
-              About
-            </a>
-          </li>
-          <li>
-            <a href="/" onClick={closeNav}>
-              Discover
-            </a>
-          </li>
-          <li>
-            <a href="/" onClick={closeNav}>
-              Get Started
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <div className="header-container">
+        <img src={logo} alt="" className="logo" />
+        <button
+          onClick={toggleNav}
+          aria-label={navIsOpen ? "Close Menu" : "Open Menu"}
+          className="menu-btn"
+        >
+          <img
+            src={navIsOpen ? closeMenu : hamburger}
+            alt=""
+            className="menu-btn-icon"
+          />
+        </button>
+        <nav className={`nav${navIsOpen ? " open" : ""}`}>
+          <ul className="nav-links">
+            <li>
+              <a href="/" onClick={closeNav} className="nav-link">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="/" onClick={closeNav} className="nav-link">
+                Discover
+              </a>
+            </li>
+            <li>
+              <a href="/" onClick={closeNav} className="nav-link">
+                Get Started
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
